0639835ef551b4807763a9393161d3d2a34c1165
marinap <marishkapuzikova@gmail.com>
2022-01-27 18:03:01 -0500

    here are the changes

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -7,9 +7,9 @@ public class Main {
         CarBasics car=new CarBasics();
 
         car.color="green";
-        car.number="789";
-        car.engine="544444cc";
-        car.door="3";
+        car.number="7899";
+        car.engine="5444444cc";
+        car.door="4";
         System.out.println(car.color);
 
         
