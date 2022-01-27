5ad5b35c29eb965866e2546c35e39c9689658af4
marinap <marishkapuzikova@gmail.com>
2022-01-27 18:20:34 -0500

    latest commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,16 +3,17 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
-	// the basic structure
-        CarBasics car=new CarBasics();
+        // the basic structure
+        CarBasics car = new CarBasics();
 
-        car.color="green";
-        car.number="7899";
-        car.engine="5444444cc";
-        car.door="4";
+
+        car.color = "green";
+        car.number = "7899";
+        car.engine = "5444444cc";
+        car.door = "4";
         System.out.println(car.color);
 
-        
-
     }
+
+
 }
---
+++ src/com/syntax/headlights.java
@@ -0,0 +1,7 @@
+package com.syntax;
+
+public class headlights {
+    String power;
+    String color;
+    String size;
+}
