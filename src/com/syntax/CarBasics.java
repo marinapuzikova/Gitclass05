cfc177b8619ca9a1352e7c439ea86eeaa365dc2c
marinap <marishkapuzikova@gmail.com>
2022-01-27 15:58:10 -0500

    The basic structure of car is ready without any features

---
+++ src/com/syntax/CarBasics.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class CarBasics {
+    String color;
+    String engine;
+    String door;
+    String number;
+}
+
---
+++ src/com/syntax/Main.java
@@ -0,0 +1,10 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// the basic structure
+        CarBasics car=new CarBasics();
+        
+    }
+}
